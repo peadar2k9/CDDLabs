7d3d79e911f03d66cfc41198c7726c6922816ffb
Peter Cronin <peadarcronin1@gmail.com>
2018-05-12 19:15:18 +0100

    Update rendezvous.cpp

--- Lab2/rendezvous.cpp
+++ Lab2/rendezvous.cpp
@@ -1,31 +1,53 @@
+/*! 
+\author Peter Cronin
+\date 12/10/2017
+\email peadarcronin1@gmail.com
+\file Semaphore.cpp
+\brief Semaphore class
+\license GNU (General Public License)
+*/
+
 #include "Semaphore.h"
 #include <iostream>
 #include <thread>
+#include <vector>
 
-void taskOne(std::shared_ptr<Semaphore> aArrived, std::shared_ptr<Semaphore> bArrived){
-  std::cout <<"Started Task 1 \n";
-  aArrived->Signal();
-  std::cout << "Task 2 Waiting \n";
-  bArrived -> Wait();
-  std::cout << "Task 1 Done \n";
+int size = 20;
+
+void taskOne(std::shared_ptr<Semaphore> aArrived, std::shared_ptr<Semaphore> bArrived, int threadNum){
+  std::cout <<"Thread " + std::to_string(threadNum) + " arrived & waiting" << std::endl;  
+  aArrived->Wait();
+  std::cout << "Thread " + std::to_string(threadNum) + " released" <<std::endl;
+  Arrived->Signal();
 }
-void taskTwo(std::shared_ptr<Semaphore> aArrived, std::shared_ptr<Semaphore> bArrived){
-  std::cout <<"Started Task 2 \n";  
-  bArrived->Signal();
-  std::cout << "Task 1 Waiting \n";
-  aArrived -> Wait();
-  std::cout << "Task 2 Done \n";
+
+void taskTwo(std::shared_ptr<Semaphore> aArrived, std::shared_ptr<Semaphore> bArrived, int threadNum){
+  std::cout <<"Thread " + std::to_string(threadNum) + " arrived & signalling" << std::endl;  
+  aArrived->Wait();
+  bArrived -> Wait();
+  std::cout << "Thread " + std::to_string(threadNum) + " goes next" <<std::endl;
+}
+
+void startVector(std::vector<std::thread> &threads, std::shared_ptr<Semaphore> aArrived, std::shared_ptr<Semaphore> bArrived){
+    for (int i = 0; i < size; i++){
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        if (i < 5){
+            std::thread current_thread = std::thread(firstThreadTask, aArrived, bArrived, i);
+            threads.push_back(move(current_thread));
+        }else{
+            std::thread current_thread = std::thread(secondThreadTask, aArrived, bArrived, i);
+            threads.push_back(move(current_thread));
+        }
+    }
 }
 
 int main(void){
-  std::thread threadOne, threadTwo;
   std::shared_ptr<Semaphore> aArrived( new Semaphore);
   std::shared_ptr<Semaphore> bArrived( new Semaphore);
-  /**< Launch the threads  */
-  threadOne=std::thread(taskTwo, aArrived, bArrived);
-  threadTwo=std::thread(taskOne, aArrived, bArrived);
-  std::cout << "Launched from the main\n";
-  threadOne.join();
-  threadTwo.join();
+  std::vector<std::thread> threads;
+  startVector(threads,aArrived, bArrived);
+  for(std::thread &thread: threads){
+    thread.join();
+  }
   return 0;
 }
