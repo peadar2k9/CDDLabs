1849e6706e43bab23887116e6f83b0203db037d3
peadarcronin <peadarcronin1@gmail.com>
2018-05-14 14:46:56 +0100

    Tidied Lab 2

--- Lab2/rendezvous.cpp
+++ Lab2/rendezvous.cpp
@@ -1,6 +1,6 @@
 /*! 
 \author Peter Cronin
-\date 12/10/2017
+\date 13/10/2017
 \email peadarcronin1@gmail.com
 \file Semaphore.cpp
 \brief Semaphore class
@@ -12,26 +12,44 @@
 #include <thread>
 #include <vector>
 
-int size = 20;
+int size = 8;
+
+/*!
+\fn void taskOne(std::shared_ptr<Semaphore> aArrived, std::shared_ptr<Semaphore> bArrived, int threadNum)
+\brief Function to be called by first thread
+\param barriers aArrived and bArrived passed in
+*/
 
 void taskOne(std::shared_ptr<Semaphore> aArrived, std::shared_ptr<Semaphore> bArrived, int threadNum){
-  std::cout <<"Thread " + std::to_string(threadNum) + " arrived & waiting" << std::endl;  
-  aArrived->Wait();
-  std::cout << "Thread " + std::to_string(threadNum) + " released" <<std::endl;
-  bArrived->Signal();
+  std::cout << "Thread " + std::to_string(threadNum) + " arrived & waiting" << std::endl;  
+  aArrived -> Wait();
+  std::cout << "Thread " + std::to_string(threadNum) + " released" << std::endl;
+  bArrived -> Signal();
 }
 
+/*!
+\fn void taskTwo(std::shared_ptr<Semaphore> aArrived, std::shared_ptr<Semaphore> bArrived, int threadNum)
+\brief Function to be called by second thread
+\param barriers aArrived and bArrived passed in
+*/
+
 void taskTwo(std::shared_ptr<Semaphore> aArrived, std::shared_ptr<Semaphore> bArrived, int threadNum){
-  std::cout <<"Thread " + std::to_string(threadNum) + " arrived & signalling" << std::endl;  
-  aArrived->Wait();
+  std::cout << "Thread " + std::to_string(threadNum) + " arrived & signalling" << std::endl;  
+  aArrived -> Signal();
   bArrived -> Wait();
-  std::cout << "Thread " + std::to_string(threadNum) + " goes next" <<std::endl;
+  std::cout << "Thread " + std::to_string(threadNum) + " goes next" << std::endl;
 }
+
+/*!
+\fn void startVector(std::vector<std::thread> &threads, std::shared_ptr<Semaphore> aArrived, std::shared_ptr<Semaphore> bArrived)
+\brief Initialise vector and evently add thread jobs
+\param vector of threads, aArrived(Semaphore) & bArrived(Semaphore)
+ */
 
 void startVector(std::vector<std::thread> &threads, std::shared_ptr<Semaphore> aArrived, std::shared_ptr<Semaphore> bArrived){
     for (int i = 0; i < size; i++){
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        if (i < 5){
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        if (i < 4){
             std::thread current_thread = std::thread(taskOne, aArrived, bArrived, i);
             threads.push_back(move(current_thread));
         }else{
@@ -40,6 +58,11 @@ void startVector(std::vector<std::thread> &threads, std::shared_ptr<Semaphore> a
         }
     }
 }
+
+/*!
+\fn int main()
+\brief Main function
+*/
 
 int main(void){
   std::shared_ptr<Semaphore> aArrived( new Semaphore);
